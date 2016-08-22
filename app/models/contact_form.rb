d703d1b4cc619716482800f9f30e1296df33a11b
Austin Reichardt <areich6@austins-mbp.attlocal.net>
2016-08-22 12:08:16 -0500

    removed uneeded files

---
+++ app/models/contact_form.rb
@@ -0,0 +1,18 @@
+class ContactForm < MailForm::Base
+
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Interested Visitor",
+      :to => "info@gowigit.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+
+
+
+end
